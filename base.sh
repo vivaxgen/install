87ec1a1a8d579d93b4fa71ee9bd1466240ec9e16
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2023-11-30 03:47:51 +0930

    fix env varible name

--- base.sh
+++ base.sh
@@ -119,12 +119,12 @@ python3 << EOF
 
 import pathlib, os
 
-path = pathlib.Path("${BINDIR}").resolve()
-activation_file = path / "activate.sh"
+BASEDIR = pathlib.Path("${BASEDIR}").resolve()
+activation_file = BASEDIR / 'bin' / "activate.sh"
 with open(activation_file, "w") as out:
   out.write('# -- base activation script from install/base.sh --\n\n')
-  out.write('export VVG_BASEDIR=${BASEDIR}\n')
-  out.write('export MAMBA_ROOT_PREFIX=${BASEDIR}/opt/umamba\n')
+  out.write(f'export VVG_BASEDIR={BASEDIR}\n')
+  out.write('export MAMBA_ROOT_PREFIX=\${VVG_BASEDIR}/opt/umamba\n')
   out.write('PATH=\${VVG_BASEDIR}/bin:\${PATH}\n')
   out.write('eval "\$(micromamba shell hook -s posix)"\n\n')
   out.write('micromamba activate ${uMAMBA_ENVNAME}\n\n')
